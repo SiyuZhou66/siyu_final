2d7449df470928405de2d9d7d6460061eafde114
Siyu Zhou <twilightzt@email.com>
2019-12-10 16:15:37 +0800

    final

---
+++ css/scriptlib2.js
@@ -0,0 +1,64 @@
+c3.generate$(document).ready(function(){
+
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+    
+
+     $("button#get_data2").click(function() {
+      var items = [];
+      var i = 0;
+      var airtable_read_endpoint = "https://api.airtable.com/v0/appV95KJaTFeqTYea/Table%201?api_key=keyfIJmVVuHtf9e2u";
+      var dataSet = [];
+      $.getJSON(airtable_read_endpoint, function(result) {
+             $.each(result.records, function(key,value) {
+                 items = [];
+                     items.push(value.fields.Name);
+                     items.push(value.fields.total);
+                     dataSet.push(items);
+                     console.log(items);
+              }); // end .each
+              console.log(dataSet);
+
+           $('#table2').DataTable( {
+               data: dataSet,
+               retrieve: true,
+               columns: [
+                   { title: "Name",
+                     defaultContent:""},
+                   { title: "total",
+                       defaultContent:"" },
+               ]
+           } );
+
+           var chart = c3.generate({
+                data: {
+                    columns: dataSet,
+                    type : 'bar'
+                },
+                axis: {
+                  x: {label: 'Name'},
+                  y: {label: 'total'}
+                },
+                bar: {
+                    title: "total by name Category:",
+                }
+            });
+
+      }); // end .getJSON
+
+   }); // end button
+
+}); // document ready
